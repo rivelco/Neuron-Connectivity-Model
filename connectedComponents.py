4137d067c6693013f621f461573e4cfb719fa473
Ricardo VC <rivel_co@protonmail.com>
2020-08-21 13:57:45 -0500

    Improved comments for the code

--- connectedComponents.py
+++ connectedComponents.py
@@ -1,16 +1,16 @@
 import math
 import numpy as np
 
-# This funtion find the connected components from a given probability matrix and criterion
+# This funtion find the connected components from a given binary matrix
 # Returns a dict of lists where each list holds the nodes it contains, also a relation of nodes and components
 def connectedComponents(matrix):
-    size = int(math.sqrt(matrix.size))       # Total amount of nodes in matrix, given that is squared
+    size = int(math.sqrt(matrix.size))             # Total amount of nodes in matrix, given that is squared
     nodes = np.full(size, -1)                      # List that indicates the [i] node belongs to the nodes[i] coponent
 
     queue = []                      # Queue for the nodes to process on the BFS
     compCounter = 0                 # Counter for the total connectet components
-    components = {}                 # Dict of lists, the 'key' holds the list of nodes of that 'key'. key is always a number
-    components[0] = []            # This component holds the isolated nodes, nodes of component '0'
+    components = {}                 # Dict of lists, the key holds the list of nodes of that key. key is always a number
+    components[0] = []              # This component holds the isolated nodes, nodes of component 0
 
     for node in range(1, size):       # For each node on the matrix
         if nodes[node] == -1:           # If we have not visited the node previously
@@ -26,11 +26,11 @@ def connectedComponents(matrix):
                         components[key] = []            # we create a new list for that key
                     components[key].append(current)     # add the current node to the list it belongs to
                     for comparedNode in range(1, size): # Iterates over all the nodes that are connected to current
-                        # If that connection is valid given a criterion and we have not visited that node before
+                        # If that connection exists and we have not visited that node before
                         if matrix[current][comparedNode] == 1 and nodes[comparedNode] == -1:
-                            flag = True # Flag true when a new connection for 'current' is found
+                            flag = True                 # Flag true when a new connection for 'current' is found
                             queue.append(comparedNode)  # we add that node to the queue
-            if not flag:  # False when no connections has been found, it means a connected component of 1 node
+            if not flag:            # False when no connections has been found, it means a connected component of 1 node
                 compCounter -= 1        # Take one out from the counter because we're correcting here
                 del components[key]     # Delete the entry from the dict
                 components[0].append(current) # Add that isolated node to the corresponding key
@@ -42,12 +42,34 @@ def connectedComponents(matrix):
 # Returns a list of the amount of nodes of every connected component
 def nodesPerCC(components):
     size = len(components)
-    nodespcc = np.empty(size)                                   # New list that holds the number of nodes per cc
+    nodespcc = np.empty(size)            # New list that holds the number of nodes per cc
     for i in range(size):                # Iterates over all the components on the dict
         nodespcc[i] = len(components[i])      # Add each number of elements per cc
     return nodespcc                                 # Returns the list of number of nodes per cc
 
+# Function that receives a dict of lists of connected components
+# Returns a list of the amount of nodes of every connected component without the isolated cells
+def nodesPerCC_WOZ(components):
+    size = len(components)
+    nodespcc = np.empty(size - 1)           # New list that holds the number of nodes per cc
+    for i in range(1, size):                # Iterates over all the components on the dict
+        nodespcc[i-1] = len(components[i])      # Add each number of elements per cc
+    return nodespcc                         # Returns the list of number of nodes per cc
+
+# Returns a numpy array indicating that the node i has edgesPN[i] edges
+# Needs a binary matrix
+def edgesPerNode(matrix):
+    size = int(math.sqrt(matrix.size))          # Get the size of the squared matrix
+    edgesPN = np.zeros(size)                    # Fill with zeros
+    for i in range(size):                       # Analyze the whole matrix
+        for j in range(size):
+            if not i == j:                      # Does not count the diagonal
+                if matrix[i][j] == 1:           # If a connection is found
+                    edgesPN[i] += 1                 # Count that edge in the node
+    return edgesPN                              # Return that array
+
 # Returns the average number of nodes per cc given a dict of components
+# Deprecated since this data is not normally distributed
 def avgNodesPCC(components):
     nodespcc = nodesPerCC(components)   # Get the number on nodes per cc
     avg = 0                             # Initialize the result
@@ -57,16 +79,8 @@ def avgNodesPCC(components):
     avg = avg / (len(components)-1)     # Calculate the average
     return avg                          # Returns that number
 
-def edgesPerNode(matrix):
-    size = int(math.sqrt(matrix.size))
-    edgesPN = np.zeros(size)
-    for i in range(size):
-        for j in range(size):
-            if not i == j:
-                if matrix[i][j] == 1:
-                    edgesPN[i] += 1
-    return edgesPN
-
+# Retrurns the average number of edges per node
+# Deprecated since the distribution usually is not normal
 def avgEdgesPN(matrix):
     edgesPN = edgesPerNode(matrix)
     avg = 0
@@ -75,6 +89,3 @@ def avgEdgesPN(matrix):
     avg -= edgesPN[0]
     avg = avg /(len(edgesPN)-1)
     return avg
-
-def nullModel(nNodes):
-    pass
--- getData.py
+++ getData.py
@@ -4,30 +4,29 @@ import random
 #import numpy as np
 
 class Cell:                 # Cell object
-    def __init__(self, x, y, name, slice, radius):  # Constructor, coords, name and slice
+    def __init__(self, x, y, name, slice, radius):  # Constructor, coords, name, slice and radius
         self.x = int(x)
         self.y = int(y)
         self.name = int(name)
         self.slice = int(slice)
         self.radius = int(radius)
 
-def extractCells(location, wichOnes): # Returns a dict with cells (Cell class)
-    cells = []                          # Final dictionary
-    numOfCell = 0
-    maxY = 0
-    cells.append(Cell(0, 0, 0, 0, 0)) # Foo cell, at cells[0]
+def extractCells(location, wichOnes): # Returns a list with cells (Cell class)
+    cells = []                          # Final list
+    numOfCell = 0                       # Used to know the total amount of desired cells
+    maxY = 0                            # Max value on y axis, because ImageJ get this inverted
+    cells.append(Cell(0, 0, 0, 0, 0))   # Foo cell, at cells[0]
 
     with open(location) as file:        # Open the file at given location
         reader = csv.DictReader(file)   # Starts a reader on dict form for csv
         for row in reader:              # Reads out each fow
-            if row['Slice'] == wichOnes or wichOnes == 'all':
-                numOfCell += 1
-                radius = random.randint(125, 150)
-                cell = Cell(row['X'], row['Y'], numOfCell, int(row['Slice']), radius)
-                cells.append(cell)
-                #cells[str(numOfCell)] = cell      # Adds that instance at dict with key 'N' NOTE that is a str
-            if int(row['Y']) > maxY:
+            if row['Slice'] == wichOnes or wichOnes == 'all':   # Select the disired section
+                numOfCell += 1                                  # Count that cell
+                radius = random.randint(125, 150)               # Choose a random radius value
+                cell = Cell(row['X'], row['Y'], numOfCell, int(row['Slice']), radius)   # Create a cell
+                cells.append(cell)                              # Add that new cell to dict
+            if int(row['Y']) > maxY:                            # Select the max value for y
                 maxY = int(row['Y'])
-    for cell in cells:
-        cell.y = abs(cell.y - maxY)
-    return cells        # Returns the final dict
+    for cell in cells:                  # Iterate over all cells
+        cell.y = abs(cell.y - maxY)         # Invert the y axis
+    return cells                        # Returns the final list
